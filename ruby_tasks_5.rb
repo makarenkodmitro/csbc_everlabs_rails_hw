2f9d588bb780e7cbd49b9814daddb37eeb152ea9
makarenkodmyto <113174764+makarenkodmitro@users.noreply.github.com>
2025-05-10 22:32:15 +0300

    Add files via upload

---
+++ ruby_tasks_5.rb
@@ -0,0 +1,16 @@
+require 'json'
+
+file = File.read('users.json')
+users = JSON.parse(file)
+
+total_age = 0
+
+users.each do |user|
+  puts "Name: #{user['name']}, Age: #{user['age']}"
+  total_age += user['age']
+end
+
+if users.any?
+  average_age = total_age.to_f / users.size
+  puts "\nAverage Age: #{average_age.round(2)}"
+end
